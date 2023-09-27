795ed6299f81b807d71f37b88a094340f1078603
kitkat-001 <54290919+kitkat-001@users.noreply.github.com>
2023-09-27 15:30:31 -0400

    added move method and more properties

--- Chess/src/chess/Piece.java
+++ Chess/src/chess/Piece.java
@@ -1,11 +1,22 @@
 package chess;
 
 public abstract class Piece {
-    private Square _square;
+    private Square _position;
+    private boolean _isWhite;
+    private boolean _hasMoved = false;
 
-    public Piece(Square square) {
-        _square = square;
+    public Piece(Square position, boolean isWhite) {
+        _position = position;
+        _isWhite = isWhite;
     }
 
     public abstract boolean isValidMove(Square destination);
+
+    public void move(Square destination) {
+        if (isValidMove(destination))
+        {
+            _position = destination;
+            _hasMoved = true;
+        }
+    }
 }
