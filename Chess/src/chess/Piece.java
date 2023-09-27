bda6710d8f27042d2c19aad00be8c7ca4df082dc
kitkat-001 <54290919+kitkat-001@users.noreply.github.com>
2023-09-27 15:39:31 -0400

    comments!

--- Chess/src/chess/Piece.java
+++ Chess/src/chess/Piece.java
@@ -1,19 +1,24 @@
 package chess;
 
+/** Abstract base class for all pieces. */
 public abstract class Piece {
-    public static Board Board;
+    /** The board shared for all Pieces */
+    public static Board Board = new Board();
 
     private Square _position;
     private boolean _isWhite;
     private boolean _hasMoved = false;
 
+    /** Creates a new piece with the desired position and color. */
     public Piece(Square position, boolean isWhite) {
         _position = position;
         _isWhite = isWhite;
     }
 
+    /** Returns whether or not moving the piece to the given destination would be valid. */
     public abstract boolean isValidMove(Square destination);
 
+    /** Moves the piece to the given destination if valid. */
     public void move(Square destination) {
         if (isValidMove(destination))
         {
